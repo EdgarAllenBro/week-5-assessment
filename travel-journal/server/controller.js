ad3247f28c6959ddd39a9ef9dca437ded5203d0c
Cameron Grieder <cjgrieder@gmail.com>
2022-01-07 09:25:50 -0700

    completed through setp 6

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,3 +1,15 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres', 
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
 
 
 module.exports = {
@@ -11,7 +23,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            CREATE TABLE cities (
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR 
+                rating INTEGER
+                country_id INTEGER REFERENCES countries('country_id')
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
@@ -212,6 +229,26 @@ module.exports = {
         `).then(() => {
             console.log('DB seeded!')
             res.sendStatus(200)
-        }).catch(err => console.log('error seeding DB', err))
+        }).catch(err => console.log('error seeding DB', err));
+        
+    },
+    getCountries: (req,res) =>{
+        sequelize.query(`
+        SELECT * FROM countries`)
+        .then(dbres => (res.status(200).send(dbres[0])))
+        .catch(err => console.log(err));
+        },
+    createCity: (req,res) =>{
+        sequelize.query(`
+        INSERT INTO cities(name, rating, country_id)
+        VALUES(${name},${rating}, ${countryId})`)
+        .then(dbres => (res.status(200).send(dbres[0])))
+        .catch(err => console.log(err))
+    },
+    getCities: (req,res) =>{
+        sequelize.query(`SELECT city_id name rating country_id FROM cities ci JOIN countries co ON ci.country_id = co.country_id
+            ORDER BY rating DESC`)
+        .then(dbres => (res.status(200).send(dbres[0])))
+        .catch(err => console.log(err))
     }
 }
